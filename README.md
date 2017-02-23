41da68f730a0d0f090c008e8d8b9de1f34733fbd
zhaobo <zhaobod1@163.com>
2017-02-23 12:01:18 +0800

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# 艺博利不锈钢橱柜，不锈钢台面厂
+* YiBoLi Stainless Steel Cabinets 
+* 网址： yiboliqd.com
+    
+     
--- data/common.inc.php
+++ data/common.inc.php
@@ -2,10 +2,17 @@
 //数据库连接信息
 $cfg_dbhost = 'localhost';
 $cfg_dbname = 'yiboliqd_db';
+$cfg_dbuser = 'root';
+$cfg_dbpwd = 'huo15com';
+$cfg_dbprefix = 'dede_';
+$cfg_db_language = 'utf8';
+
+/*$cfg_dbhost = '211.149.214.242';
+$cfg_dbname = 'yiboliqd_db';
 $cfg_dbuser = 'yiboliqd';
 $cfg_dbpwd = 'huo15_ybl';
 $cfg_dbprefix = 'dede_';
-$cfg_db_language = 'utf8';
+$cfg_db_language = 'utf8';*/
 
 
 ?>
